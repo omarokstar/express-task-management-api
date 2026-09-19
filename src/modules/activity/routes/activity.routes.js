8a8508508e93d0542762ed5799a5d62ac834be44
OmarOkstar <omarokstar2001@gmail.com>
2026-09-19 23:40:49 +0300

    fix: wrap activity routes in asyncHandler

--- src/modules/activity/routes/activity.routes.js
+++ src/modules/activity/routes/activity.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
+const asyncHandler = require('../../../middleware/asyncHandler');
 const c = require('../controllers/activity.controller');
 
 const activityRouter = express.Router();
 
-activityRouter.get('/', c.get_activity);
-activityRouter.post('/', c.addActivity);
+activityRouter.get('/', asyncHandler(c.get_activity));
+activityRouter.post('/', asyncHandler(c.addActivity));
 
 module.exports = activityRouter;
